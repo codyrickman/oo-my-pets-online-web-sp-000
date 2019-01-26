e2da3e88f6ce350dbecd2f491214399e36db575c
Cody Rickman <cody@creativemindstudios.com>
2019-01-26 15:08:52 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -23,6 +23,10 @@ class Owner
     new_cat = Cat.new(name)
     @pets[:cats] << new_cat
   end
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
   def self.all
     @@all
   end
