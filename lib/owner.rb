5128889c0e339c8133cb3c1ec805258f84cb3998
Cody Rickman <cody@creativemindstudios.com>
2019-01-26 14:31:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,11 @@
 class Owner
   attr_reader :pets, :name
+  @@all = []
   def initialize(name)
     @name = name
     @pets = {fishes: [], cats: [], dogs: []}
+  end
+  def all
+    @@all
   end
 end
